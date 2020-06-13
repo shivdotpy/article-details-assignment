2406e1f0c8d8dfe7477eeed8b47870698b104910
Abhishek Joshi <abhishek.joshi@nagarro.com>
2020-06-13 13:24:36 +0530

    apply some changes related to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
+
 app.use(express.static(__dirname + '/build'));
 
 app.get('*', (request, response) => {
